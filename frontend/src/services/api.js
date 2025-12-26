27f8e9f7ef1785245799b9517831f21ba9645f2e
Nikhil Anand <nikhilanand2202@gmail.com>
2025-12-26 22:46:34 +0530

    new api file  for vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,21 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:8000/api",
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) req.headers.Authorization = `Bearer ${token}`;
+//   return req;
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use((req) => {
